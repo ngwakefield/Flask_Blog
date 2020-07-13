c4c8b4cdf77442f4d14f5188ac770ea12bfef6ef
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-13 04:36:45 -0500

    Create layout.html, incorporate bootstrap inherit into home and about.html

--- flask_blog.py
+++ flask_blog.py
@@ -7,17 +7,33 @@ $env:FLASK_APP = "flask_blog.py"
 
 ctrl-C to end webserver
 '''
+# Dummy data
+posts = [
+    {
+        'author':'Nick Wakefield',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted': '2020-07-13'
+    },
+    {
+        'author':'Jane Doe',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted': '2020-08-01'
+    }
+]
+
 
 # Basically the root page of the home page
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', posts = posts)
 
 # Create an about page
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title = "About")
 
 
 # If python calls this file directly, this will run
