a3d1ad2e8dc12de83b177cb84cb174764092779a
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-13 18:42:37 -0500

    Add login user to db; hash password; import bcrypt to init

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0907aeb5e25a61b08f7c15770f467f64'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flask_blog import routes
