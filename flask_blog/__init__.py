f9e84f7622baec975efe444314df30f5ee86f597
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-13 18:54:52 -0500

    Add login manager package, and loader in models.py

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0907aeb5e25a61b08f7c15770f467f64'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flask_blog import routes
