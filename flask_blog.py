7079fec38f83e6c421d8ea808d805a0f84daec0c
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-13 16:23:18 -0500

    Added POST request functionality; fixed hello_world routing --> home Display results on main page after form submit

--- flask_blog.py
+++ flask_blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -29,7 +29,7 @@ posts = [
 # Basically the root page of the home page
 @app.route('/')
 @app.route('/home')
-def hello_world():
+def home():
     return render_template('home.html', posts = posts)
 
 # Create an about page
@@ -38,9 +38,12 @@ def about():
     return render_template('about.html', title = "About")
 
 # Create Register and Login pages
-@app.route('/register')
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title = "Register", form = form)
 
 @app.route('/login')
