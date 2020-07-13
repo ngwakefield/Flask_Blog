9683feb2db67873981415a90c79da8178d22b166
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-13 04:09:24 -0500

    Add an about route; main function handling; notes

--- flask_blog.py
+++ flask_blog.py
@@ -4,10 +4,23 @@ app = Flask(__name__)
 '''
 Powershell syntax for environment variable
 $env:FLASK_APP = "flask_blog.py"
+
+ctrl-C to end webserver
 '''
 
 # Basically the root page of the home page
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1> Hello, World! I am running Flask. </h1>'
 
+# Create an about page
+@app.route('/about')
+def about():
+    return '<h1> About Page. </h1>'
+
+
+# If python calls this file directly, this will run
+# Don't need env variable
+if __name__ == '__main__':
+    app.run(debug = True)
