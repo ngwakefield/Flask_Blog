ee647b02f873001db3772077f2c3cbc4d5818083
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-14 01:20:45 -0500

    Add in password reset request route and template

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request, abort
 from flask_blog import app, db, bcrypt
-from flask_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from flask_blog.forms import (RegistrationForm, LoginForm, 
+UpdateAccountForm, PostForm, RequestResetForm, 
+ResetPasswordForm)
 from flask_blog.models import User, Post
 from flask_login import login_user, logout_user, current_user, login_required
 from PIL import Image
@@ -160,3 +162,10 @@ def user_posts(username):
         .order_by(Post.date_posted.desc())\
         .paginate(per_page=3, page = page)
     return render_template('user_posts.html', posts = posts, user = user)
+
+@app.route("/reset_password", methods = ['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    return render_template('reset_request.html', title = 'Reset Password', form = form)
