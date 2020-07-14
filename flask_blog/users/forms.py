ea2a67ca413d01218173951dda34600bdd679724
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-14 03:41:03 -0500

    Move all forms out of forms.py into subdirs

---
+++ flask_blog/users/forms.py
@@ -0,0 +1,86 @@
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+    validators = [DataRequired(), Length(min=2, max =20)])
+    email = StringField('Email', 
+    validators = [DataRequired(), Email()])
+    password = PasswordField('Password',
+    validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+    validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        '''
+        Check if username already exists
+        '''
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            # Enters unless user is None
+            raise ValidationError('That username is taken. Please choose a different one.')
+    
+    def validate_email(self, email):
+        '''
+        Check if email already exists
+        '''
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            # Enters unless user is None
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+    validators = [DataRequired(), Email()])
+    password = PasswordField('Password',
+    validators = [DataRequired()])
+    remember = BooleanField('Remember Me') 
+    submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+    validators = [DataRequired(), Length(min=2, max =20)])
+    email = StringField('Email', 
+    validators = [DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators = [FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        '''
+        Check if username already exists
+        Only do this if the username is being changed
+        '''
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                # Enters unless user is None
+                raise ValidationError('That username is taken. Please choose a different one.')
+        
+    def validate_email(self, email):
+        '''
+        Check if email already exists
+        Only do this if the email is being changed
+        '''
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                # Enters unless user is None
+                raise ValidationError('That email is taken. Please choose a different one.')
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        '''
+        Check if email does not exist
+        '''
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            # Enters unless user is None
+            raise ValidationError('There is no account with that email. Please register first.')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password',
+    validators = [DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+    validators = [DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
