f4e78d7476fa83cccb6ca077c23e11e5d30781fc
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-13 20:49:57 -0500

    Add account change functionality; add default profile pic

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', 
@@ -39,3 +40,32 @@ class LoginForm(FlaskForm):
     validators = [DataRequired()])
     remember = BooleanField('Remember Me') 
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', 
+    validators = [DataRequired(), Length(min=2, max =20)])
+    email = StringField('Email', 
+    validators = [DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        '''
+        Check if username already exists
+        Only do this if the username is being changed
+        '''
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                # Enters unless user is None
+                raise ValidationError('That username is taken. Please choose a different one.')
+        
+    def validate_email(self, email):
+        '''
+        Check if email already exists
+        Only do this if the email is being changed
+        '''
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                # Enters unless user is None
+                raise ValidationError('That email is taken. Please choose a different one.')
