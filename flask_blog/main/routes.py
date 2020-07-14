fa5ee1ebd71757e2e18d47d8ceb618a07ad320a5
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-14 03:39:33 -0500

    Move all routes out of routes.py into subdir versions

---
+++ flask_blog/main/routes.py
@@ -0,0 +1,18 @@
+from flask import Blueprint
+
+main = Blueprint('main', __name__)
+
+
+# Basically the root page of the home page
+@main.route('/')
+@main.route('/home')
+def home():
+    # Page is looking up the URL for query params
+    page = request.args.get('page', 1, type = int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=3, page = page)
+    return render_template('home.html', posts = posts)
+
+# Create an about page
+@main.route('/about')
+def about():
+    return render_template('about.html', title = "About")
