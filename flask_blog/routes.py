55fa9fbea0e95f2cd76badcb6eca4e9aa87b6c4f
Nick Wakefield <n.g.wakefield@gmail.com>
2020-07-13 19:01:24 -0500

    Add login functionality

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flask_blog import app, db, bcrypt
 from flask_blog.forms import RegistrationForm, LoginForm
 from flask_blog.models import User, Post
-
+from flask_login import login_user, current_user
 
 # Dummy data
 posts = [
@@ -35,6 +35,8 @@ def about():
 # Create Register and Login pages
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -47,12 +49,17 @@ def register():
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        # Get the user email from the form
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            # Log the user in
+            login_user(user, remember = form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccesful. Please check username and password', 'danger')
+            flash('Login Unsuccesful. Please check email and password', 'danger')
     
     return render_template('login.html', title = "Log In", form = form)
